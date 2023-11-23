b0e8a9049b8aed6fb914790439797f6250b9ebe2
Anders Hausvik <andykodehode@gmail.com>
2023-11-23 11:31:10 +0100

    liten koselig tekst

--- fontenetest/App.js
+++ fontenetest/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Øyvind er en flott fyr</Text>
       <StatusBar style="auto" />
     </View>
   );
